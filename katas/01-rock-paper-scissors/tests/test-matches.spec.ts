9c1c2356d062833a64a1892866541ed1064621dd
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2021-03-11 22:18:48 +0100

    style: prettier the core game specification file

--- katas/01-rock-paper-scissors/tests/test-matches.spec.ts
+++ katas/01-rock-paper-scissors/tests/test-matches.spec.ts
@@ -6,13 +6,13 @@ import {
   MatchEngine,
   Player,
   StandardGame,
-  StandardMatchEngine
-} from "@game";
+  StandardMatchEngine,
+} from '@game';
 
 const weaponRules = {
-  rock: "scissors",
-  paper: "rock",
-  scissors: "paper",
+  rock: 'scissors',
+  paper: 'rock',
+  scissors: 'paper',
 };
 const nullLogger: Logger = { log: () => 0 };
 const matchEngine: MatchEngine = new StandardMatchEngine(
@@ -25,75 +25,75 @@ let p1: Player;
 let p2: Player;
 beforeAll(() => {
   game = new StandardGame(matchEngine);
-  p1 = createPlayer("player1");
-  p2 = createPlayer("player2");
+  p1 = createPlayer('player1');
+  p2 = createPlayer('player2');
 });
 
 const createPlayer = (id: string): Player => ({ id });
 const createMove = (playerID: string, weaponID: string): GameMove => ({
   player: { id: playerID },
-  weapon: { id: weaponID },
+  weapon: { id: weaponID, label: '' },
 });
 
-it("should return that rock beats scissors", () => {
-  const p1Move = createMove(p1.id, "rock");
-  const p2Move = createMove(p2.id, "scissors");
+it('should return that rock beats scissors', () => {
+  const p1Move = createMove(p1.id, 'rock');
+  const p2Move = createMove(p2.id, 'scissors');
   const turn: GameTurn = {
     playerMoves: [p1Move, p2Move],
   };
 
   const outcome = game.evaluateMatch(turn);
 
-  expect(outcome.type).toBe("winning");
+  expect(outcome.type).toBe('winning');
   expect(outcome.winningPlayer).toBe(p1.id);
 });
 
-it("should return that scissors beats paper", () => {
-  const p1 = createPlayer("player1");
-  const p2 = createPlayer("player2");
-  const p1Move = createMove(p1.id, "paper");
-  const p2Move = createMove(p2.id, "scissors");
+it('should return that scissors beats paper', () => {
+  const p1 = createPlayer('player1');
+  const p2 = createPlayer('player2');
+  const p1Move = createMove(p1.id, 'paper');
+  const p2Move = createMove(p2.id, 'scissors');
   const turn: GameTurn = {
     playerMoves: [p1Move, p2Move],
   };
 
   const outcome = game.evaluateMatch(turn);
 
-  expect(outcome.type).toBe("winning");
+  expect(outcome.type).toBe('winning');
   expect(outcome.winningPlayer).toBe(p2.id);
 });
 
-it("should return that paper beats rock", () => {
-  const p1 = createPlayer("player1");
-  const p2 = createPlayer("player2");
-  const p1Move = createMove(p1.id, "paper");
-  const p2Move = createMove(p2.id, "rock");
+it('should return that paper beats rock', () => {
+  const p1 = createPlayer('player1');
+  const p2 = createPlayer('player2');
+  const p1Move = createMove(p1.id, 'paper');
+  const p2Move = createMove(p2.id, 'rock');
   const turn: GameTurn = {
     playerMoves: [p1Move, p2Move],
   };
 
   const outcome = game.evaluateMatch(turn);
 
-  expect(outcome.type).toBe("winning");
+  expect(outcome.type).toBe('winning');
   expect(outcome.winningPlayer).toBe(p1.id);
 });
 
-it("should return a draw", () => {
-  const p1 = createPlayer("player1");
-  const p2 = createPlayer("player2");
-  const p1Move = createMove(p1.id, "paper");
-  const p2Move = createMove(p2.id, "paper");
+it('should return a draw', () => {
+  const p1 = createPlayer('player1');
+  const p2 = createPlayer('player2');
+  const p1Move = createMove(p1.id, 'paper');
+  const p2Move = createMove(p2.id, 'paper');
   const turn: GameTurn = {
     playerMoves: [p1Move, p2Move],
   };
 
   const outcome = game.evaluateMatch(turn);
 
-  expect(outcome.type).toBe("draw");
+  expect(outcome.type).toBe('draw');
 });
 
-describe("smoke tests", () => {
-  it("should create a new game", () => {
+describe('smoke tests', () => {
+  it('should create a new game', () => {
     const gameInstance = new StandardGame(matchEngine);
     expect(gameInstance).toBeTruthy();
   });
